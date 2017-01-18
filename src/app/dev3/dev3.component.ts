df2386851bb4858384050e93e1b43314f8f02761
Mafields <Mafields86@gmail.com>
2017-01-18 10:15:29 +0200

    After login, save & show token and username

--- src/app/dev3/dev3.component.ts
+++ src/app/dev3/dev3.component.ts
@@ -8,6 +8,7 @@ import {HttpapiService} from "../httpapi.service";
 })
 export class Dev3Component implements OnInit {
 
+  loggedinMode: boolean = false;
   signInMode: boolean = true;
   linkText: string = 'Don\'t have an account?';
 
@@ -22,7 +23,12 @@ export class Dev3Component implements OnInit {
     password: ''
   };
 
-  changeMode() {
+  loggedinUser =  {
+    username: '',
+    token: ''
+  };
+
+  changeSignupMode() {
     if (this.signInMode === true) {
       this.signInMode = false;
       this.linkText = 'Already have an account?'
@@ -44,6 +50,9 @@ export class Dev3Component implements OnInit {
     console.log("Singin! start");
     this.httpApi.post("login", this.signinUser).subscribe(response => {
       console.log(response);
+      this.loggedinUser.username = this.signinUser.username;
+      this.loggedinUser.token = response.token;
+      this.loggedinMode = true;
     });
     console.log("Singin! end");
   }
