ff6d4c176d297e346cc76f9b02b0e402a9c26845
Mafields <Mafields86@gmail.com>
2017-01-18 09:12:42 +0200

    Signin & signup works!

--- src/app/dev3/dev3.component.ts
+++ src/app/dev3/dev3.component.ts
@@ -15,12 +15,25 @@ export class Dev3Component implements OnInit {
     email:''
   };
 
+  signinUser = {
+    username: '',
+    password: ''
+  };
+
   private onSignup() {
     console.log("Singup! start");
     this.httpApi.post("users", this.newUser).subscribe(response => {
       console.log(response);
     });
     console.log("Singup! end");
+  }
+
+  private onSignin() {
+    console.log("Singin! start");
+    this.httpApi.post("login", this.signinUser).subscribe(response => {
+      console.log(response);
+    });
+    console.log("Singin! end");
   }
 
 
