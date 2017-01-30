d85717071516ae3668d6ae5aa403e79aa63952cb
Mafields <Mafields86@gmail.com>
2017-01-30 13:33:04 +0200

    upload form & replace of auth-box & user-info-box

--- src/app/dev3/dev3.component.ts
+++ src/app/dev3/dev3.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpapiService} from "../httpapi.service";
 
 @Component({
@@ -9,14 +9,79 @@ import {HttpapiService} from "../httpapi.service";
 export class Dev3Component implements OnInit {
 
   TODO: boolean = true;
+  JWT_USER: string = 'dao_user';
 
-  private resetJson (json){
+  typeFile: boolean = true;
+  typeYoutube: boolean = false;
+  groupNew: boolean = true;
+  groupOld: boolean = false;
+  oldGroups = [];
+  oldGroupsSelected: any = 0;
+  oldGroupsSelectedTag: string = '';
+
+
+  languages: any = [
+    {"name": "Java", "value": "java"},
+    {"name": "C", "value": "c"},
+    {"name": "C++", "value": "cpp"},
+    {"name": "C#", "value": "cs"},
+    {"name": "Php", "value": "php"},
+    {"name": "SQL", "value": "sql"},
+    {"name": "HTML", "value": "html"},
+    {"name": "HTML5", "value": "html5"},
+    {"name": "Css", "value": "css"},
+    {"name": "JavaScript", "value": "javascript"},
+    {"name": "Angular", "value": "angular"},
+    {"name": "React", "value": "react"}
+  ]
+
+
+  private resetJson(json) {
     for (let i in json) {
       json[i] = '';
     }
   }
 
-  constructor(private httpApi: HttpapiService) { }
+  oldGroupsSelectedChange() {
+    this.oldGroupsSelectedTag = this.oldGroups[this.oldGroupsSelected].tag;
+  }
+
+  typeChange(event) {
+    let name = event.target.name;
+    if (name == "file") {
+      if (this.typeFile) {
+        this.typeYoutube = false;
+      } else {
+        this.typeYoutube = true;
+      }
+    } else if (name == "youtube") {
+      if (this.typeYoutube) {
+        this.typeFile = false;
+      } else {
+        this.typeFile = true;
+      }
+    } else if (name == "gNew") {
+      if (this.groupNew) {
+        this.groupOld = false;
+      } else {
+        this.groupOld = true;
+      }
+    } else if (name == "gOld") {
+      if (this.groupOld) {
+        this.groupNew = false;
+      } else {
+        this.groupNew = true;
+      }
+    }
+  }
+
+  constructor(private httpApi: HttpapiService) {
+    let userN = window.localStorage.getItem(this.JWT_USER);
+    this.httpApi.get(`/users/${userN}/groups`).subscribe(response => {
+      console.log(response);
+      this.oldGroups = response;
+    });
+  }
 
   ngOnInit() {
   }
