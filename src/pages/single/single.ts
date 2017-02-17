43bc73230b72986acb7da5a39c2f3459159cd72b
tundet <tundet@metropolia.fi>
2017-02-17 13:12:18 +0200

    comment

--- src/pages/single/single.ts
+++ src/pages/single/single.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 import {HttpApi} from "../../providers/http-api";
 
 /*
-  Generated class for the Single page.
+ Generated class for the Single page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
   selector: 'page-single',
   templateUrl: 'single.html'
 })
 export class SinglePage {
 
+  private comments;
+  private txtcomment: string = '';
+  private commentInfo;
   private mediaInfo;
   private textMediaContent;
   api_url: string = 'https://dao-api.othnet.ga/uploads/original/';
@@ -21,6 +24,7 @@ export class SinglePage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private httpApi: HttpApi) {
     let id = this.navParams.get('id');
     console.log(id);
+
     this.httpApi.get(`media/${id}`).subscribe(response => {
       this.mediaInfo = response;
       console.log(this.mediaInfo);
@@ -31,10 +35,30 @@ export class SinglePage {
         })
       }
     });
+
+    this.httpApi.get(`media/${id}/comments`).subscribe(response => {
+      this.comments = response;
+      console.log(this.comments);
+    });
   }
+
+  comment(event: any) {
+    this.commentInfo = {
+      medium_id: this.mediaInfo.id,
+      Comment: this.txtcomment
+    };
+    console.log(this.commentInfo);
+    this.httpApi.post("comments", this.commentInfo).subscribe(response => {
+      console.log(response);
+    })
+    console.log(this.comments);
+    this.txtcomment = "";
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SinglePage');
+
   }
 
 }
