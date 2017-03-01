e9cb747a7c1177857281c9ae72562dcf871cc20b
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-03-01 09:20:29 +0200

    Remove console.logs, clean and document code

--- src/components/like-box/like-box.ts
+++ src/components/like-box/like-box.ts
@@ -11,75 +11,78 @@ export class LikeBoxComponent {
   private mediumId: number;
   private userId: string;
   private likeId: number;
+
   private mediumHasBeenLiked: boolean;
   private mediumHasBeenDisliked: boolean;
 
+  /**
+   * LikeBoxComponent constructor
+   *
+   * @param httpApi Injected HttpApi service
+   * @param navParams Injected NavParams
+   */
   constructor(private httpApi: HttpApi, public navParams: NavParams) {
     this.mediumHasBeenLiked = false;
-    this.mediumHasBeenDisliked;
+    this.mediumHasBeenDisliked = false;
 
     this.userId = localStorage.getItem('dao_user_id');
     this.mediumId = this.navParams.get('id');
 
     this.httpApi.get(`media/${this.mediumId}/likes`).subscribe(response => {
-      for (var like in response) {
-        console.log('user id: ' + this.userId);
-        console.log('user id of like: ' + response[like]['user_id']);
+      for (let like in response) {
+        if (response.hasOwnProperty(like)) {
+          if (response[like]['user_id'] == this.userId) {
+            if (response[like]['user_id'] == 1) {
+              this.mediumHasBeenLiked = true;
+            } else {
+              this.mediumHasBeenDisliked = true;
+            }
 
-        if (response[like]['user_id'] == this.userId) {
-          if (response[like]['user_id'] == 1) {
-            this.mediumHasBeenLiked = true;
-          } else {
-            this.mediumHasBeenDisliked = true;
+            this.likeId = response[like]['id'];
+
+            break;
           }
-
-          this.likeId = response[like]['id'];
-
-          console.log('medium has been liked');
-
-          break;
+        } else {
+          // TODO: Show a toast telling the like could not be registered.
         }
-
-        console.log('medium has not been liked');
       }
     });
-
-    console.log('medium has been liked: ' + this.mediumHasBeenLiked);
   }
 
+  /**
+   * Like a medium.
+   */
   like() {
-    console.log('liked');
-
     if (this.mediumHasBeenLiked || this.mediumHasBeenDisliked) {
       this.removeLike();
     }
 
     this.httpApi.post("likes", { medium_id: this.mediumId, like: 1 }).subscribe(response => {
-      console.log(response);
-
       this.likeId = response.id;
       this.mediumHasBeenLiked = true;
     });
   }
 
+  /**
+   * Dislike a medium.
+   */
   dislike() {
     if (this.mediumHasBeenLiked || this.mediumHasBeenDisliked) {
       this.removeLike();
     }
 
     this.httpApi.post("likes", { medium_id: this.mediumId, like: 0 }).subscribe(response => {
-      console.log(response);
-
       this.likeId = response.id;
       this.mediumHasBeenDisliked = true;
 
     });
   }
 
+  /**
+   * Remove like from a medium.
+   */
   removeLike() {
-    this.httpApi.delete(`likes/${this.likeId}`).subscribe(response => {
-      console.log(response);
-
+    this.httpApi.delete(`likes/${this.likeId}`).subscribe(() => {
       this.mediumHasBeenLiked = false;
       this.mediumHasBeenDisliked = false;
     });
