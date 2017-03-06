df4241c2ec9410ece06d7800ce72060055cdde91
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-03-06 09:30:43 +0200

    Add comments and documentation

--- src/pipes/get-username.ts
+++ src/pipes/get-username.ts
@@ -1,26 +1,39 @@
-import {Injectable, Pipe} from '@angular/core';
-import {HttpApi} from "../providers/http-api";
+import { Injectable, Pipe } from '@angular/core';
+import { HttpApi } from "../providers/http-api";
 
-/*
- Get username of the given user ID.
+/**
+ * Get username matching the given user ID.
  */
 @Pipe({
   name: 'GetUsername',
-  pure: true
 })
 @Injectable()
 export class GetUsername {
 
+  /**
+   * GetUsername constructor.
+   *
+   * @param httpApi Injected HttpApi service
+   */
   constructor(private httpApi: HttpApi) {
+
   }
 
   private username: string;
 
-  transform(value: any, args?: any): any {
-    this.httpApi.get('users/' + value).subscribe(response => {
+  /**
+   * Return username that matches the given user ID.
+   *
+   * @param {int} value ID of the user
+   * @returns {string} Username
+   */
+  transform(value: number): any {
+    return this.httpApi.get('users/' + value).subscribe(response => {
       this.username = response.username;
+
+      console.log('pipe response: ' + this.username);
+      return this.username;
     });
 
-    return this.username;
   }
 }
